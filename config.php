e5b5c366edab7e2428e8d13fcfa36b41d81fbc00
Tai LE <tai.le-cr@rockyou.com>
2016-04-27 23:33:17 +0700

    installation complete

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/hello-opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/hello-opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/XAMPP/xamppfiles/htdocs/hello-opencart/catalog/');
+define('DIR_SYSTEM', '/Applications/XAMPP/xamppfiles/htdocs/hello-opencart/system/');
+define('DIR_LANGUAGE', '/Applications/XAMPP/xamppfiles/htdocs/hello-opencart/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/XAMPP/xamppfiles/htdocs/hello-opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/XAMPP/xamppfiles/htdocs/hello-opencart/system/config/');
+define('DIR_IMAGE', '/Applications/XAMPP/xamppfiles/htdocs/hello-opencart/image/');
+define('DIR_CACHE', '/Applications/XAMPP/xamppfiles/htdocs/hello-opencart/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/XAMPP/xamppfiles/htdocs/hello-opencart/system/download/');
+define('DIR_UPLOAD', '/Applications/XAMPP/xamppfiles/htdocs/hello-opencart/system/upload/');
+define('DIR_MODIFICATION', '/Applications/XAMPP/xamppfiles/htdocs/hello-opencart/system/modification/');
+define('DIR_LOGS', '/Applications/XAMPP/xamppfiles/htdocs/hello-opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'hello-opencart');
+define('DB_PREFIX', 'oc_');
